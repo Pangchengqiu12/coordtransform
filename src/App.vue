2727627bfb9f7f01ac05a9a06c88781309df1886
lw <2212261902@qq.com>
2025-01-14 17:28:12 +0800

    优化布局

--- src/App.vue
+++ src/App.vue
@@ -39,50 +39,51 @@
           />
         </el-select>
       </div>
-      <div class="upload">
-        <el-button type="primary" @click="openFileDialog">选择文件</el-button>
-        <el-button type="primary" @click="openFolderDialog"
-          >打开文件所在位置</el-button
-        >
-        <el-button type="primary" @click="convertFiles">转换文件</el-button>
+      <div class="point">
+        <el-input
+          v-model="point"
+          style="width: 200px"
+          placeholder="输入经度,纬度。例如:116.0,39.0"
+        />
+        <div class="svg">
+          <svg
+            t="1736821848019"
+            class="icon"
+            viewBox="0 0 1024 1024"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            p-id="6967"
+            width="22"
+            height="32"
+          >
+            <path
+              d="M950.857143 665.6H73.142857a43.885714 43.885714 0 0 1 0-87.771429h789.942857L696.32 355.474286a43.885714 43.885714 0 0 1 70.217143-52.662857l219.428571 292.571428a43.885714 43.885714 0 0 1-35.108571 70.217143z"
+              p-id="6968"
+            ></path>
+          </svg>
+        </div>
+        <el-input
+          :value="pointResult"
+          style="width: 200px; margin-right: 12px"
+          placeholder="Please input"
+          disabled
+        />
+        <el-button type="primary" @click="convertPoint">转换</el-button>
       </div>
     </div>
-    <div class="point">
-      <el-input
-        v-model="point"
-        style="width: 300px"
-        placeholder="输入经度,纬度。例如:116.0,39.0"
-      />
-      <div class="svg">
-        <svg
-          t="1736821848019"
-          class="icon"
-          viewBox="0 0 1024 1024"
-          version="1.1"
-          xmlns="http://www.w3.org/2000/svg"
-          p-id="6967"
-          width="22"
-          height="32"
-        >
-          <path
-            d="M950.857143 665.6H73.142857a43.885714 43.885714 0 0 1 0-87.771429h789.942857L696.32 355.474286a43.885714 43.885714 0 0 1 70.217143-52.662857l219.428571 292.571428a43.885714 43.885714 0 0 1-35.108571 70.217143z"
-            p-id="6968"
-          ></path>
-        </svg>
-      </div>
-      <el-input
-        :value="pointResult"
-        style="width: 300px; margin-right: 12px"
-        placeholder="Please input"
-        disabled
-      />
-      <el-button type="primary" @click="convertPoint">转换</el-button>
-    </div>
-    <div class="fileHeader">
-      <div class="saveFolder">{{ saveFolder }}</div>
-      <el-button type="primary" @click="selectSaveFolder"
-        >选择保存文件夹</el-button
+
+    <div class="upload">
+      <el-button type="primary" @click="openFileDialog">选择文件</el-button>
+      <el-button type="primary" @click="openFolderDialog"
+        >打开文件所在位置</el-button
       >
+      <el-button type="primary" @click="convertFiles">转换文件</el-button>
+      <div class="fileHeader">
+        <el-button type="primary" @click="selectSaveFolder"
+          >选择保存文件夹</el-button
+        >
+        <div class="saveFolder">{{ saveFolder }}</div>
+      </div>
     </div>
     <div class="result">
       <el-table :data="fileList">
@@ -124,7 +125,7 @@ const openFileDialog = async () => {
 };
 // 转换单个点
 const convertPoint = async () => {
-  if (sourceCRS.value === targetCRS.value) {
+  if (sourceCRS.value === targetCRS.value || !point.value) {
     return;
   }
   if (sourceCRS.value === 'WGS84') {
@@ -219,21 +220,19 @@ onMounted(() => {
   align-items: center;
 }
 .fileHeader {
+  flex: 1;
   display: flex;
   align-items: center;
-  justify-content: flex-end;
-  width: 100%;
-  margin-top: 12px;
+  margin-left: 12px;
 }
 .saveFolder {
   max-width: 500px;
   overflow: hidden;
   text-overflow: ellipsis;
   white-space: nowrap;
-  margin-right: 12px;
+  margin-left: 12px;
 }
 .point {
-  width: 100%;
   display: flex;
   align-items: center;
 }
@@ -246,9 +245,7 @@ onMounted(() => {
   overflow: auto;
   margin-top: 12px;
 }
-.upload-demo {
-  margin: 20px 0;
-}
+
 .row {
   width: 100%;
   display: flex;
@@ -257,12 +254,15 @@ onMounted(() => {
   margin-bottom: 20px;
 }
 .row > .select {
-  margin-right: 20px;
+  margin-right: 12px;
   display: flex;
-  width: 380px;
+  align-items: center;
+  width: 250px;
   justify-content: space-around;
-  margin-right: 20px;
 }
-.row > .upload {
+.upload {
+  display: flex;
+  align-items: center;
+  width: 100%;
 }
 </style>
